3ba395a57232bba39ffbbb7bfd9fc8f808f0e410
neuro-nest-ai <neuronestai.23@gmail.com>
2024-02-08 23:52:23 +0530

    updated

--- twilio_configure.py
+++ twilio_configure.py
@@ -1,27 +1,33 @@
 import logging
-
 from twilio.rest import Client
 from decouple import config
 
-account_sid=config("TWILIO_ACCOUNT_SID")
-auth_token=config("TWILIO_AUTH_TOKEN")
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-client=Client(account_sid,account_sid)
-twilio_number=config("TWILIO_NUMBER")
+# Get Twilio credentials from environment variables
+account_sid = config("TWILIO_ACCOUNT_SID")
+auth_token = config("TWILIO_AUTH_TOKEN")
+twilio_number = config("TWILIO_NUMBER")
 to_number = config("TO_NUMBER")
 
-logging.basicConfig(level=logging.INFO)
-logger=logging.getLogger(__name__)
-client = Client(account_sid,auth_token)
+# Initialize Twilio client
+client = Client(account_sid, auth_token)
 
 def send_message(body_text):
     try:
-        message=client.messages.create(
+        # Send message using Twilio client
+        message = client.messages.create(
             from_=f"whatsapp:{twilio_number}",
             body=body_text,
             to=f"whatsapp:{to_number}"
         )
-        return
-        logger.info(f"Message sent to {to_number}:{message.body}")
+        # Log message sent
+        logger.info(f"Message sent to {to_number}: {message.body}")
     except Exception as e:
-        logger.error(f"Error sending message to {to_number}:{e}")
+        # Log error if message sending fails
+        logger.error(f"Error sending message to {to_number}: {e}")
+
+
+
