783ff44becffff73337ba81696c5d6deeb86e736
neuro-nest-ai <neuronestai.23@gmail.com>
2024-02-08 22:34:40 +0530

    up code

--- Treasurer/Reply.py
+++ Treasurer/Reply.py
@@ -2,9 +2,9 @@ import re
 from twilio.rest import Client
 import os
 
-TWILIO_ACCOUNT_SID = os.getenv['TWILIO_ACCOUNT_SID']
-TWILIO_AUTH_TOKEN= os.getenv['TWILIO_AUTH_TOKEN']
-TWILIO_PHONE_NUMBER = os.getenv['TWILIO_NUMBER']
+#TWILIO_ACCOUNT_SID = os.getenv['TWILIO_ACCOUNT_SID']
+#TWILIO_AUTH_TOKEN= os.getenv['TWILIO_AUTH_TOKEN']
+#TWILIO_PHONE_NUMBER = os.getenv['TWILIO_NUMBER']
 
 
 # Your regex pattern for extracting amount, phone numbers, and trust
@@ -24,14 +24,14 @@ class SenderConfig:
         trust=message[2]
         return UPI_traction_id,trust
 
-    def send_twilio_sms(self, to, body):
-        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    #def send_twilio_sms(self, to, body):
+    #client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-        message = client.messages.create(
-            to=to,
-            from_=TWILIO_PHONE_NUMBER,
-            body=body)
-        return message
+    #message = client.messages.create(
+    #to=to,
+    #from_=TWILIO_PHONE_NUMBER,
+    #body=body)
+        #return message
 
 
 class ReplyConfig:
