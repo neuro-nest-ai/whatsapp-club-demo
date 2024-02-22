a089627b0cbce472f1c7fd3ecc37778f27c7dc87
unknown <praveenkumar.ganesan@rap.ventures>
2024-02-23 03:56:42 +0530

    fix: full setup done

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import logging
-from twilio_configure import send_message,get_image
-from twilio.twiml.messaging_response import MessagingResponse
 import requests
 from main import generate_answer,image_details
 import io
+import json
 import uuid
+import os
+from dotenv import load_dotenv
 
 
 
@@ -14,6 +15,8 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+load_dotenv()
 
 
 
@@ -25,91 +28,95 @@ def userFirstLogin(mobileNumber,profileName):
     users.append({'name':'praveen','mobile_number':'9486706677'})
     # print(users)
     return True
-
-
-
-def get_path(media_url,DOWNLOAD_DIRECTORY):
-    image = get_image(media_url[0])
-    unique_filename = str(uuid.uuid4()) + ".jpeg"
-    with open(f'{DOWNLOAD_DIRECTORY}/{unique_filename}','wb') as f:
-        f.write(image.content)
-        image_path = f'{DOWNLOAD_DIRECTORY}/{unique_filename}'
-    return image_path
-
-DOWNLOAD_DIRECTORY = 'Image_data'  
     
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    print(request.json)
     if request.method == 'POST':
-        mode = request.args.get('hub.mode')
-        token = request.args.get('hub.verify_token')
-        challenge = request.args.get('hub.challenge')
-        
-        
-    
-        if mode == 'subscribe' and token == "123456":
-            return challenge, 200
+        data = request.json
+        entryFromRequest = data['entry'][0]
+        value =  entryFromRequest['changes'][0]['value']
+        if('statuses' in value): 
+            return jsonify({'status': 'success'}), 200
         else:
-            return 'Verification failed', 403
+            messages = value['messages'][0]
+            if(messages['type'] == 'text'):
+                body_of_text = messages['text']['body']
+                answer=generate_answer(body_of_text)
+                url = os.getenv('base_url') + '/messages'
+                headers = {
+                    'Authorization': f'Bearer {os.getenv('AUTHTOKEN')}',
+                    'Content-Type': 'application/json'
+                }
+                data = {
+                    "messaging_product": "whatsapp",
+                    "recipient_type": "individual",
+                    "to": messages['from'],
+                    "type": "text",
+                    "text": {
+                        "body": answer
+                    }
+                }
+
+                response = requests.post(url, headers=headers, json=data)
+                return jsonify({'status': 'success'}), 200
+            else:
+                body_of_text = messages['image']
+                id_image = body_of_text['id']
+                url = os.getenv('image_url') + id_image + '/'
+                headers = {'Authorization': f'Bearer {os.getenv('AUTHTOKEN')}'}
+
+                response = requests.get(url, headers=headers)
+                response_content_str = response.content.decode('utf-8')
+                response_data = json.loads(response_content_str)
+                image_url = response_data['url']
+                image_download = requests.get(image_url.replace("\\/", "/"),headers=headers) 
+
+                if image_download.status_code == 200:
+                    if not os.path.exists(f'{os.getenv('DOWNLOAD_DIRECTORY')}/{id_image}'):
+                        os.makedirs(os.getenv('DOWNLOAD_DIRECTORY') + '/' + id_image)
+                        print("Folder created successfully.")
+                    else:
+                        print("Folder already exists.")
+                    with open(f'{os.getenv('DOWNLOAD_DIRECTORY')}/{id_image}/{id_image}.jpg', 'wb') as f:
+                        f.write(image_download.content)
+                        print("Media file downloaded successfully.")
+                else:
+                    print("Failed to download media file. Status code:", image_download.status_code)
+                answer=image_details(f'{os.getenv('DOWNLOAD_DIRECTORY')}/{id_image}/{id_image}.jpg')
+                url = os.getenv('base_url') + '/messages'
+                headers = {
+                    'Authorization': f'Bearer {os.getenv('AUTHTOKEN')}',
+                    'Content-Type': 'application/json'
+                }
+                data = {
+                    "messaging_product": "whatsapp",
+                    "recipient_type": "individual",
+                    "to": messages['from'],
+                    "type": "text",
+                    "text": {
+                        "body": answer
+                    }
+                }
+
+                response = requests.post(url, headers=headers, json=data)
+                return jsonify({'status': 'success'}), 200
+
+
     elif request.method == 'GET':
         mode = request.args.get('hub.mode')
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
     
-        if mode == 'subscribe' and token == "123456":
+        if mode == 'subscribe' and token == os.getenv('verifyToken'):
             return challenge, 200
         else:
             return 'Verification failed', 403
          
      
-    
-    
-    
-    
-    
-    
-
-
-
-@app.route('/welcome',methods=['POST'])
-def home():
-    #incoming_que = request.form['message']  # Assuming 'message' is the name of the input field containing user messages
-
-    #incoming_request = request.form.to_dict(flat=False)
-    incoming_que = request.values.get('Body', '').lower()
-    print("Question: ", request.form.to_dict(flat=False))
-    
-    
-    if request.values['NumMedia'] != '0':
-        media = request.form.to_dict(flat=False)
-        media_url = media['MediaUrl0']
-    #media_to = media['From'][0]
-    
-    #media_type = media['MediaContentType0']
-    #message_sent_by_user = incoming_request['Body'][0]
-    # Generate the answer using GPT-3
-    #answer = generate_answer(incoming_que)
-    
-    #if(userFirstLogin(incoming_request['From'][0],incoming_request['ProfileName'][0])):
-    #print("hii",users)
-        #return "ok"
-   
-    
-        if media_url:
-            try:
-                image_path=get_path(media_url,DOWNLOAD_DIRECTORY)
-                print("Image saved at:", image_path)
-                answer=image_details(image_path)
-            except Exception as e:
-                logger.error(f"Error while creating the image : {e}")
-    else:
-        answer=generate_answer(incoming_que)
-        print("BOT Answer: ", answer)
-        
-    bot_resp = MessagingResponse()
-    msg = bot_resp.message()
-    msg.body(answer)
-    return str(bot_resp)
 if __name__ == '__main__':
+    if not os.path.exists(os.getenv('DOWNLOAD_DIRECTORY')):
+        os.makedirs(os.getenv('DOWNLOAD_DIRECTORY'))
+        print("Folder created successfully.")
+    else:
+        print("Folder already exists.")
     app.run(host='0.0.0.0', debug=False, port=8000) 
